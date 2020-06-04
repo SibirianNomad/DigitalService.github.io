151be0d79233a98fd7e1ddfab53c852aa962003d
SibirianNomad <SibirianNomad@gmail.com>
2020-06-04 09:39:40 +0700

    new

--- index.php
+++ index.php
@@ -4,11 +4,19 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<title>Digital Service</title>
 <meta name='viewport' content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-	
+<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" >
 </head>
 <body>
-
-	
+	<div class='header'>
+		<div class='b-block'>
+			<div class='logo_block'>
+				<img class='logo' src='img/logo1.png'>
+				<div class='logo_title'>
+					<span class='logo_title_first'>Digital</span>
+					<span class='logo_title_second'>Service</div>
+			</div>
+		</div>
+	</div>
 			
 			
 
