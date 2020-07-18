1b6508cfc0c6d532aa3676a7ae8e48b9acc38f47
SibirianNomad <SibirianNomad@gmail.com>
2020-07-18 08:27:06 +0700

    added three sections

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-
+$(document).ready(function(){   
+    PopUpHide();
+});
 $('.nav-toggle').on('click',function(){
     if($('.burger-menu').hasClass('display-none')){
         $('.burger-menu').removeClass('display-none');
@@ -6,3 +8,26 @@ $('.nav-toggle').on('click',function(){
         $('.burger-menu').addClass('display-none');
     }
 });
+
+$(function(){$("#phone").mask("8(999)-999-9999");})
+$(function(){$("#popup_phone").mask("8(999)-999-9999");})
+
+function PopUpShow(){
+    $("#popup").show();
+    
+}
+function PopUpHide(){
+
+    $("#popup").hide();
+}
+
+$('.drop_bid').click(function(){
+    $.fancybox.open({
+        src : '#popup',
+        opts : {
+            touch : false,
+        }
+    })
+});
+
+
