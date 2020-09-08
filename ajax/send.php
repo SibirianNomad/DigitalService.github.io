21886a8fce5f62dd32428b2f443804bfd1598573
Eugenek <eugenek@evaxsoft.com>
2020-09-08 17:59:19 +0700

    mail

--- ajax/send.php
+++ ajax/send.php
@@ -26,31 +26,49 @@ if(isset($_POST['number_popup'])){
     $number=trim($_POST['number_popup']);
 }
 if(!empty($name) && !empty($number)){
-
-    $mail = new PHPMailer();
-
-    $mail -> isSMTP();   
-    $mail -> CharSet = "UTF-8";
-    $mail -> SMTPAuth   = true;
-    $mail -> Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
-
-    $mail -> Host       = 'smtp.gmail.com'; 
-    $mail -> Username   = 'sibiriannomad@gmail.com';
-    $mail -> Password   = 'Faraon25'; 
-    $mail -> SMTPSecure = 'ssl';
-    $mail -> Port       = 465;
-    $mail -> setFrom('sibiriannomad@gmail.com', 'Заявка от клиента'); 
-    $mail -> addAddress('office@dgs.ru'); 
-    
-    $mail -> isHTML(true);
-    $mail -> Subject = 'Заявка от '.$name;
-    $mail -> Body = '<p><b>Имя: </b>'.$name.'</p><p><b>Телефон: </b>'.$number.'</p>
+    $headers =  'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'From: Your name <info@address.com>' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $subject = 'Заявка от '.$name;
+    $body = '<p><b>Имя: </b>'.$name.'</p><p><b>Телефон: </b>'.$number.'</p>
     <p><b>Тема сообщения: </b>'.$theme.'</p><p><p><b>Сообщение: </b>'.$maintext.'</p><p>';
-    if(!$mail->send()) {
+    $mail = mail('office@dgs.ru', $subject, $body, $headers);
+    if(!$mail) {
         echo 'Message could not be sent.';
-        echo 'Mailer Error: ' . $mail->ErrorInfo;
-       } else {
+    } else {
         echo 1;
-       }  
-   
+    }
+//проверяем отправку
+//    $mail = new PHPMailer();
+//
+//    $mail -> isSMTP(true);
+//
+//    $mail->SMTPDebug = 0;
+//
+//    $mail->Debugoutput = 'html';
+//
+//    $mail->SMTPSecure = 'ssl';
+//
+//    $mail->SMTPAuth = true;
+//
+//
+//    $mail -> Host       = 'smtp.gmail.com';
+//    $mail -> Username   = 'sibiriannomad@gmail.com';
+//    $mail -> Password   = 'Faraon25';
+//    $mail -> Port       = 465;
+//    $mail -> setFrom('sibiriannomad@gmail.com', 'Заявка от клиента');
+//    $mail -> addAddress('ekurchenko@yahoo.com');
+//
+//
+//    $mail -> Subject = 'Заявка от '.$name;
+//    $mail -> Body = '<p><b>Имя: </b>'.$name.'</p><p><b>Телефон: </b>'.$number.'</p>
+//    <p><b>Тема сообщения: </b>'.$theme.'</p><p><p><b>Сообщение: </b>'.$maintext.'</p><p>';
+//    if(!$mail->send()) {
+//        echo 'Message could not be sent.';
+//        echo 'Mailer Error: ' . $mail->ErrorInfo;
+//    } else {
+//        echo 1;
+//    }
+
+
 }
