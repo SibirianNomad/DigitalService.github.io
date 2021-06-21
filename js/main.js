3e084b677f755808d9450fdb3bcc2c232d2cd55b
Eugene <sibiriannomad@gmail.com>
2021-06-21 16:06:07 +0700

    edited widget js

--- js/main.js
+++ js/main.js
@@ -138,22 +138,22 @@ $('.license').on('click', function () {
     }
 
 });
-$('#popup_form_widget').submit(function () {
-    if ($(this).find('input.error').length == 0) {
-        $('.contact-left_button').attr('disabled', true);
-        var number =  $('#popup_phone_widget').val();
-        var i = $('#i').val();
-        number = Number.parseInt(number.replace(/[^\d]/g, ''));
-        console.log(typeof (number))
-        $.ajax({
-            url: "https://213.183.125.35/wcb.php",
-            type: 'post',
-            data: {p: number, i: i},
-            cache: false
-        },function (data, status) {
-            console.log(status,status)
-        })
-    }
+$(document).ready(function() {
+    $('#popup_form_widget').on('click',function () {
+        if ($(this).find('input.error').length == 0) {
+            $('.contact-left_button').attr('disabled', true);
+            var number = $('#popup_phone_widget').val();
+            var i = $('#i').val();
+            number = Number.parseInt(number.replace(/[^\d]/g, ''));
+            $.ajax({
+                url: "https://213.183.125.35/wcb.php",
+                type: 'post',
+                data: {p: number, i: i},
+                cache: false
+            }, function (data, status) {
+                console.log(data, status)
+            })
+        }
 
+    });
 });
-
