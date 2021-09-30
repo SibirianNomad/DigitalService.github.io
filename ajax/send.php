df2d23cafc0417fba5f201278d8efc2511f071e7
Eugene <sibiriannomad@gmail.com>
2021-09-30 12:18:40 +0700

    edited smtp config

--- ajax/send.php
+++ ajax/send.php
@@ -28,30 +28,28 @@ if(isset($_POST['number_popup'])){
 if(!empty($name) && !empty($number)){
     try{
     $mail = new PHPMailer();
-    $mail->isSMTP();
-    $mail->SMTPDebug = 2;
-    $mail->Debugoutput = 'html';
-    $mail->Port = 587;
-    $mail->SMTPSecure = 'tls';
-    $mail->SMTPAuth = true;
-    $mail->isHTML(true);
-    $mail->CharSet ="utf-8";
 
-    $mail->Host       = 'smtp.gmail.com'; 
-    $mail->Username   = 'sibiriannomad';
-    $mail->Password = 'Faraon2506';
+        $mail->isSMTP();
 
-    $mail->setFrom('sibiriannomad@gmail.com', 'Заявка от клиента'); 
-    $mail->addAddress('office@dgs.ru','Meneger DGS'); 
-    
-    
-    $mail->Subject = 'Заявка от '.$name;
-    $mail->Body = '<p><b>Имя: </b>'.$name.'</p><p><b>Телефон: </b>'.$number.'</p>
+        $mail->Host = 'smtp-pulse.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'sibiriannomad@gmail.com';
+        $mail->Password = 'QiRadWtajCn8E3D';
+        $mail->setFrom('no-reply@dgs.ru', 'Заявка от клиента');
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = '587';
+        $mail->CharSet = 'UTF-8';
+        $mail->addAddress('ekurchenko@yahoo.com');
+        $mail->isHTML(true);
+        $mail->Subject = 'Заявка от '.$name;
+        $mail->Body = 'Hello!';
+        $mail->SMTPDebug = 1;
+            $mail->Body = '<p><b>Имя: </b>'.$name.'</p><p><b>Телефон: </b>'.$number.'</p>
     <p><b>Тема сообщения: </b>'.$theme.'</p><p><p><b>Сообщение: </b>'.$maintext.'</p><p>';
-    $mail->send();
+        $mail->send();
     echo 1;
 }catch(Exception $e) {
     echo "Ошибка отправки:".$mail->ErrorInfo;
-   
+
 }
 }
